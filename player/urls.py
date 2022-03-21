7f43c39197f4c202254b636bcb55996ea70af2dd
Tommy Han <tommy_han1@intuit.com>
2022-03-20 20:04:33 -0400

    setup details

--- player/urls.py
+++ player/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # ex: /player/
     path('', views.index, name='index'),
+    # ex: /player/5/
+    path('<int:question_id>/', views.detail, name='detail'),
 ]
