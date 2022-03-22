02bcd74683fb0ebb1c541b537278893d7fb3064b
Tommy Han <tommy_han1@intuit.com>
2022-03-22 10:08:17 -0400

    docker pkg version chgs

--- player/urls.py
+++ player/urls.py
@@ -1,14 +1,3 @@
-# from django.urls import path
-
-# from . import views
-
-# urlpatterns = [
-#     # ex: /player/
-#     path('', views.list, name='list'),
-#     # ex: /player/5/
-#     path('<str:player_id>/', views.detail, name='detail'),
-# ]
-
 
 from rest_framework import routers
 
